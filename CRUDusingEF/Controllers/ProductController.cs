14defaadd922c1cc0f60604413dcdce3ae0fe1a7
8975014275 <avgiri1998@gmail.com>
2022-10-24 12:17:53 +0530

    Add project files.

---
+++ CRUDusingEF/Controllers/ProductController.cs
@@ -0,0 +1,113 @@
+﻿using CRUDusingEF.Data;
+using CRUDusingEF.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDusingEF.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        ProductDAL productdal;
+        public ProductController(ApplicationDbContext context)
+        {
+            this.context = context;
+            productdal=new ProductDAL(context);
+
+        }
+        // GET: ProductController1
+        public ActionResult List()
+        {
+            var p=productdal.GetAllProducts();
+            return View(p);
+        }
+
+        // GET: ProductController1/Details/5
+        public ActionResult Details(int id)
+        {
+            var p = productdal.GetProductById(id);
+            return View(p);
+            
+        }
+
+        // GET: ProductController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product prod)
+        {
+            try
+            {
+                int res = productdal.AddProduct(prod);
+                if (res == 1)
+                    return RedirectToAction(nameof(List));
+                else
+                    return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var p = productdal.GetProductById(id);
+            return View(p);
+            
+        }
+
+        // POST: ProductController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product prod)
+        {
+            try
+            {
+                int res = productdal.UpdateProduct(prod);
+                if (res == 1)
+                    return RedirectToAction(nameof(List));
+                else
+                    return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var p = productdal.GetProductById(id);
+            return View(p);
+           
+        }
+
+        // POST: ProductController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirm(int id)
+        {
+            try
+            {
+                int res = productdal.DeleteProduct(id);
+                if (res == 1)
+                    return RedirectToAction(nameof(List));
+                else
+                    return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CRUDusingEF/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUDusingEF.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "name is required")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = " company name is required")]
+        public string? CompanyName { get; set; }
+        [Required(ErrorMessage = "price is required")]
+        public int Price { get; set; }
+
+    }
+}
